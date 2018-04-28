df8fbbfdb49e8253bed824e65507b5e9522d8986
Gopinaath Ragavan <33642194+gopinaath007@users.noreply.github.com>
2018-04-29 01:06:12 +1000

    rename_files updated with os listdir

---
+++ Programming_Foundations_with_Python/Prank_Project/rename_files.py
@@ -0,0 +1,18 @@
+"""
+Docstrinng:
+This script will rename a bunch of image files that have numbers in them
+"""
+
+from os import listdir
+
+def rename_files():
+    # (1) get file names from a folder
+    file_list = os.listdir(r"C:\Users\gopir\Documents\Gopi\Education\Github\Udacity\Programming_Foundations_with_Python\Prank_Project\prank")
+
+
+    # (2) for each file, rename filename
+
+
+
+
+rename_files()
