9875143ab6d9ae4f3995baf7a0af41ca01e3fd77
Gopinaath Ragavan <33642194+gopinaath007@users.noreply.github.com>
2018-04-29 12:18:41 +1000

    turtle program updated with interactive menu

--- Programming_Foundations_with_Python/Draw_Turtles/mindstorms.py
+++ Programming_Foundations_with_Python/Draw_Turtles/mindstorms.py
@@ -1,5 +1,5 @@
 """
-This program has a turtle on an orange background draw a square, circle and triangle
+This program has a turtle that will ask you whether you would like to see a drawing or a fractal
 """
 
 import turtle
@@ -39,21 +39,39 @@ def draw_art():
     draw_triangle(Sahana)
     window.exitonclick()
 
-def draw_fractal(square_count):
+
+def draw_fractal(shape_count):
+    fractal_type = int(input("This is a fractal drawing program \n Enter a number for drawing a fractal: 1 for using a square or 2 for using a triangle"))
     window = turtle.Screen()
     window.bgcolor("orange")
     Gopi = turtle.Turtle()
     Gopi.shape("turtle")
     Gopi.color("red")
-    Gopi.speed(2)
-    for i in range(square_count):
-        Gopi.right(360/square_count)
-        draw_square(Gopi)
+    Gopi.speed(2) 
+    for i in range(shape_count):
+        if fractal_type == 1:
+            Gopi.right(360/shape_count)
+            draw_square(Gopi)
+        elif fractal_type == 2:
+            Gopi.right(360/shape_count)
+            draw_triangle(Gopi)
     window.exitonclick()
 
-square_count = input("Enter number of squares for circular fractal as a positive whole number: ")   
-draw_fractal(int(square_count))
-#draw_art()
+
+def turtle_draw():
+    main_menu = int(input("Would you like to see my drawing or draw a circular fractal; Enter 1 for drawing or 2 for fractal: "))
+    menu = main_menu
+    if menu == 1:
+        draw_art()
+    elif menu == 2:
+        shape_count = int(input("Enter number of shapes for fractal as a positive whole number: "))
+        draw_fractal(shape_count)
+    elif (menu <1) or (menu >2):
+        print("Try again. Enter 1 for drawing or 2 for fractal: ")
+        turtle_draw()
+
+    
+turtle_draw()
 
 
 
