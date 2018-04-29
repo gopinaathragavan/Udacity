012c1feb817dfc0d910572370ea5638df34fef05
Gopinaath Ragavan <33642194+gopinaath007@users.noreply.github.com>
2018-04-29 14:05:34 +1000

    code optimisation and changed program to only draw fractals

--- Programming_Foundations_with_Python/Draw_Turtles/mindstorms.py
+++ Programming_Foundations_with_Python/Draw_Turtles/mindstorms.py
@@ -18,30 +18,9 @@ def draw_triangle(turtle):
     turtle.right(135)
     turtle.forward(135)
 
-
-def draw_art():
-    window = turtle.Screen()
-    window.bgcolor("orange")
-    Gopi = turtle.Turtle()
-    Gopi.shape("turtle")
-    Gopi.color("red")
-    Gopi.speed(2)
-    draw_square(Gopi)
-    Shama = turtle.Turtle()
-    Shama.shape("arrow")
-    Shama.color("blue")
-    Shama.speed(2)
-    Shama.circle(100)
-    Sahana = turtle.Turtle()
-    Sahana.shape("turtle")
-    Sahana.color("purple")
-    Sahana.speed(1)
-    draw_triangle(Sahana)
-    window.exitonclick()
-
-
-def draw_fractal(shape_count):
+def draw_fractal():
     fractal_type = int(input("This is a fractal drawing program \n Enter a number for drawing a fractal: 1 for using a square or 2 for using a triangle"))
+    shape_count = int(input("Enter number of shapes for fractal as a positive whole number: "))
     window = turtle.Screen()
     window.bgcolor("orange")
     Gopi = turtle.Turtle()
@@ -57,21 +36,8 @@ def draw_fractal(shape_count):
             draw_triangle(Gopi)
     window.exitonclick()
 
-
-def turtle_draw():
-    main_menu = int(input("Would you like to see my drawing or draw a circular fractal; Enter 1 for drawing or 2 for fractal: "))
-    menu = main_menu
-    if menu == 1:
-        draw_art()
-    elif menu == 2:
-        shape_count = int(input("Enter number of shapes for fractal as a positive whole number: "))
-        draw_fractal(shape_count)
-    elif (menu <1) or (menu >2):
-        print("Try again. Enter 1 for drawing or 2 for fractal: ")
-        turtle_draw()
-
-    
-turtle_draw()
+   
+draw_fractal()
 
 
 
