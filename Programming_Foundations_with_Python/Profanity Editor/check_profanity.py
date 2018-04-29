f90d662bd9cc24544aa66b64f864902491e6fc3a
Gopinaath Ragavan <33642194+gopinaath007@users.noreply.github.com>
2018-04-29 14:01:36 +1000

    updated read_text function

--- Programming_Foundations_with_Python/Profanity Editor/check_profanity.py
+++ Programming_Foundations_with_Python/Profanity Editor/check_profanity.py
@@ -3,7 +3,8 @@ This program checks for curse words in a piece of text and alerts the user
 """
 
 def read_text():
-    quotes = open(r"C:\Users\gopir\Documents\Gopi\Education\Github\Udacity\Programming_Foundations_with_Python\Profanity Editor\movie_quotes.txt")
+    file_path = r"C:\Users\gopir\Documents\Gopi\Education\Github\Udacity\Programming_Foundations_with_Python\Profanity Editor\movie_quotes.txt"
+    quotes = open(file_path, 'r')
     contents_of_file = quotes.read()
     print(contents_of_file)
     quotes.close()
