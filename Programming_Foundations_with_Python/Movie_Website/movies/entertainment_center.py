ebd10e9ff21c9a90df99844be19ed94ec5fd14e6
Gopinaath Ragavan <33642194+gopinaath007@users.noreply.github.com>
2018-04-29 17:37:34 +1000

    movie website project update

--- Programming_Foundations_with_Python/Movie_Website/movies/entertainment_center.py
+++ Programming_Foundations_with_Python/Movie_Website/movies/entertainment_center.py
@@ -5,4 +5,17 @@ toy_story = media.Movie("Toy Story",
                         "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
-print(toy_story.storyline)
+avatar = media.Movie("Avatar",
+                    "A marine on an alien planet",
+                    "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                    "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+avengers = media.Movie("Avengers: Infinity War",
+                    "The Avengers and their Super Hero allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos.",
+                    "https://www.movieinsider.com/images/p/600//488741_m1522889808.jpg",
+                    "https://www.youtube.com/watch?v=6ZfuNTqbHE8")
+
+#print(toy_story.storyline)
+#print(avatar.storyline)
+#avatar.show_trailer()
+avengers.show_trailer()
