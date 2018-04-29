564fea7857a8674f3e6c1f29dc8de80fc1dfbac2
Gopinaath Ragavan <33642194+gopinaath007@users.noreply.github.com>
2018-04-29 20:40:09 +1000

    inheritance update

--- Programming_Foundations_with_Python/Inheritance/inheritance.py
+++ Programming_Foundations_with_Python/Inheritance/inheritance.py
@@ -5,8 +5,8 @@ class Parent():
         self.eye_color = eye_color
 
     def show_info(self):
-        print("Last Name - "+self.last_name)
-        print("Eye Color - "+self.eye_color)
+        print("Last Name - " + self.last_name)
+        print("Eye Color - " + self.eye_color)
 
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
@@ -19,12 +19,14 @@ class Child(Parent):
             print("Eye Color - "+self.eye_color)
             print("Number of toys - "+str(self.number_of_toys)
 
-#billy_cyrus = Parent("Cyrus", "blue")
+billy_cyrus = Parent("Cyrus", "blue")
 #print(billy_cyrus.last_name)
-#billy_cyrus.show_info()
+billy_cyrus.show_info()
 
-miley_cyrus = Child("Cyrus", "Blue", 5)
+#miley_cyrus = Child("Cyrus", "Blue", 5)
 # print(miley_cyrus.last_name)
 # print(miley_cyrus.number_of_toys)
-miley_cyrus.show_info()
+#miley_cyrus.show_info()
+
+del vars
 
