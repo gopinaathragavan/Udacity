a174338b01dc112c5ce503b9ff55427eb9f0249f
Gopinaath Ragavan <33642194+gopinaath007@users.noreply.github.com>
2018-04-29 14:43:34 +1000

    added length of file check

--- Programming_Foundations_with_Python/Profanity Editor/check_profanity.py
+++ Programming_Foundations_with_Python/Profanity Editor/check_profanity.py
@@ -6,7 +6,7 @@ def read_text():
     file_path = r"C:\Users\gopir\Documents\Gopi\Education\Github\Udacity\Programming_Foundations_with_Python\Profanity Editor\movie_quotes.txt"
     quotes = open(file_path, 'r')
     contents_of_file = quotes.read()
-    print(contents_of_file)
+    print("Length of file in characters: ", len(contents_of_file), "\n", contents_of_file)
     quotes.close()
 
 read_text()
